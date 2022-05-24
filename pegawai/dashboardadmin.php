e28a13baff332a114027507296e72e18a38914de
ArRahmaan17 <ardrah17@gmail.com>
2022-05-24 10:03:34 +0700

    update bug view

--- pegawai/dashboardadmin.php
+++ pegawai/dashboardadmin.php
@@ -57,9 +57,9 @@ if ($_SESSION['role'] === 'admin') {
             <!-- Page content wrapper-->
             <div id="page-content-wrapper">
                 <!-- Top navigation-->
-                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                <nav class="navbar navbar-expand-sm navbar-light bg-light border-bottom">
                     <div class="container-fluid">
-                        <button class="btn btn-primary d-none d-sm-block d-md-none" id="sidebarToggle">
+                        <button class="btn btn-primary d-sm-block d-md-none d-lg-none" id="sidebarToggle">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                             </svg>
--- pelanggan/dashboard.php
+++ pelanggan/dashboard.php
@@ -47,7 +47,7 @@ if ($_SESSION['nama_pelanggan'] !== null) {
                 <!-- Top navigation-->
                 <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
                     <div class="container-fluid">
-                        <button class="btn btn-primary d-none d-sm-block d-md-none" id="sidebarToggle">Toggle Menu</button>
+                        <button class="btn btn-primary d-sm-block d-md-none d-lg-none" id="sidebarToggle">Toggle Menu</button>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
